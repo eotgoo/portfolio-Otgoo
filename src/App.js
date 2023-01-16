0543082b25f59df3d86d3e23499f4fc74bf47f98
Evening <evening@mstars-lab3-10s-iMac.local>
2023-01-16 17:13:51 +0800

    1.16 monday

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import NavbarMenu from "./components/Navbar";
 import About from "./components/pages/About";
 import Hero from "./components/pages/Hero";
 import Skills from "./components/pages/Skills";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavbarMenu />
-      <Hero />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
+
+      {/* <Hero />
       <About />
-      <Skills />
+      <Skills /> */}
     </div>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -22,13 +22,13 @@ const NavbarMenu = () => {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto"></Nav>
             <Nav className={Styles.navMenu}>
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#skills">Skills</Nav.Link>
-              <Nav.Link href="#education">Education</Nav.Link>
-              <Nav.Link href="#work">Work</Nav.Link>
-              <Nav.Link href="#experience">Experience</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/skills">Skills</Nav.Link>
+              <Nav.Link href="/education">Education</Nav.Link>
+              <Nav.Link href="/work">Work</Nav.Link>
+              <Nav.Link href="/experience">Experience</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
